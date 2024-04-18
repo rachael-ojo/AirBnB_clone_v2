4eed8dbd9bafd094c7fefd97b156e53a059f23df
rachael-ojo <adeolaatilade@gmail.com>
2024-04-17 21:46:28 -0700

    Updated

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Creates a database
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+USE hbnb_dev_db;
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+FLUSH PRIVILEGES
