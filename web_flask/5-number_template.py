ccf3f6bce59e7b78b1fef6cad02b5000c6c18189
rachael-ojo <adeolaatilade@gmail.com>
2024-05-18 20:15:17 -0700

    Updated

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+start Flask application
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
